420ec4da60c8749eb022f443cff38ff890a7d92d
6kV <nizarazu9@hotmail.com>
2017-09-19 22:20:38 +0200

    add aggregation for es search

--- public/sequence_controller.js
+++ public/sequence_controller.js
@@ -1,21 +1,68 @@
+import _ from 'lodash';
 import { uiModules } from 'ui/modules';
+import { AggResponseTabifyProvider } from 'ui/agg_response/tabify/tabify';
 
 import 'plugins/sequence/directives/sequenceVisualization';
 
 const module = uiModules.get('kibana/sequence', ['kibana']);
 
-module.controller('SequenceController', function ($scope, $http) {
-  $http.get('../api/sequence/indices').then((response) => {
-    $scope.indices = response.data;
-  });
+module.controller('SequenceController', function ($scope, $http, Private) {
+  const tabifyAggResponse = Private(AggResponseTabifyProvider);
 
-  $http.get('../api/sequence/schema').then((response) => {
-    $scope.schema = response.data;
-  });
+  var metrics = $scope.metrics = [];
+  var result;
+  var Flow = function (timestamp, from, to, url, message) {
+    this.timestamp = timestamp;
+    this.from = from;
+    this.to = to;
+    this.url = url;
+    this.message = message;
+  };
 
-  // $scope.$watch('esResponse', function (resp) {
-  //   if (resp) {
-  //     // console.log('lol esResponse', resp);
-  //   }
-  // });
+  var timestamp;
+  var from;
+  var to;
+  var message;
+  var url;
+
+  $scope.processTableGroups = function (tableGroups) {
+    for (var i = 0; i < tableGroups.tables[0].rows.length; i++) {
+      for (var j = 0; j < tableGroups.tables[0].rows[i].length; j++) {
+        if ( j == 0 ) {
+          timestamp = tableGroups.tables[0].rows[i][j].key;
+        }
+        if ( j == 1 ) {
+          from = tableGroups.tables[0].rows[i][j].key;
+        }
+        if ( j == 2 ) {
+          to = tableGroups.tables[0].rows[i][j].key;
+        }
+        if ( j == 3 ) {
+          url = tableGroups.tables[0].rows[i][j].key;
+        }
+        if ( j == 4 ) {
+          message = tableGroups.tables[0].rows[i][j].key;
+          result[i] = new Flow(timestamp,from,to,url,message) ;
+        }
+      }
+
+    }
+    console.log(JSON.stringify(result));
+    $scope.schema = JSON.stringify(result);
+  };
+
+  $scope.$watch('esResponse', function (resp) {
+    let tableGroups = $scope.tableGroups = null;
+    if (resp) {
+      const options = {
+        asAggConfigResults: true
+      };
+
+      metrics.length = 0;
+      tableGroups = tabifyAggResponse($scope.vis, resp, options);
+      console.log(tableGroups.tables[0].column);
+      result = new Array(tableGroups.tables[0].rows.length);
+      $scope.processTableGroups(tableGroups);
+    }
+  });
 });
