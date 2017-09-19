bbff328bf94019794a967a7cd9388a5d8e2f3fdc
6kV <nizarazu9@hotmail.com>
2017-09-19 22:20:38 +0200

    add aggregation for es search

--- public/sequence.js
+++ public/sequence.js
@@ -1,8 +1,9 @@
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
 import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+import { VisSchemasProvider } from 'ui/vis/schemas';
 
-import './less/main.less';
 import './assets/sequence.css';
+import './less/main.less';
 import 'plugins/sequence/sequence_controller';
 
 // register the provider with the visTypes registry
@@ -10,6 +11,7 @@ VisTypesRegistryProvider.register(SequenceVisProvider);
 
 function SequenceVisProvider(Private) {
   const TemplateVisType = Private(TemplateVisTypeProvider);
+  const Schemas = Private(VisSchemasProvider);
 
   // return the visType object, which kibana will use to display and configure new
   // Vis object of this type.
@@ -19,6 +21,24 @@ function SequenceVisProvider(Private) {
     description: 'Display as Sequence Chart',
     icon: 'fa-tachometer',
     template: require('plugins/sequence/templates/vis.html'),
+    schemas: new Schemas([
+      {
+        group: 'metrics',
+        name: 'metric',
+        title: 'Metric',
+        min: 1,
+        max: 1,
+        aggFilter: ['!derivative'],
+        defaults: [
+          { type: 'count', schema: 'metric' }
+        ]
+      },
+      {
+        group: 'buckets',
+        name: 'bucket',
+        title: 'Split Rows'
+      }
+    ])
   });
 }
 
